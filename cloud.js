055dd22316bae2c5c5c4b4428052b90dc07e3057
Chris Baume <chris.baume@bbc.co.uk>
2015-05-21 16:23:51 +0100

    Added more stop words.

--- cloud.js
+++ cloud.js
@@ -50,7 +50,7 @@ var background = svg.append("g"),
     //.on("change", generate);
 
 // From Jonathan Feinberg's cue.language, see lib/cue.language/license.txt.
-var stopWords = /^(stuff|hard|first|back|might|things|particular|sometimes|around|although|little|give|yes|o|even|bs\.|used|want|thing|quite|point|perhaps|basically|hundred|thousand|obviously|something|probably|going|really|already|twenty|come|please|go|got|tell|one|two|three|six|seven|eight|nine|know|often|well|please|hopefully|five|four|lot|getting|d|r|just|get|actually|also|i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$/,
+var stopWords = /^(maybe|ago|mean|oh|called|always|based|bit|set|sure|run|sort|last|able|question|thank|another|ok|kind|think|million|like|yeah|stuff|hard|first|back|might|things|particular|sometimes|around|although|little|give|yes|o|even|bs\.|used|want|thing|quite|point|perhaps|basically|hundred|thousand|obviously|something|probably|going|really|already|twenty|come|please|go|got|tell|one|two|three|six|seven|eight|nine|know|often|well|please|hopefully|five|four|lot|getting|d|r|just|get|actually|also|i|me|my|myself|we|us|our|ours|ourselves|you|your|yours|yourself|yourselves|he|him|his|himself|she|her|hers|herself|it|its|itself|they|them|their|theirs|themselves|what|which|who|whom|whose|this|that|these|those|am|is|are|was|were|be|been|being|have|has|had|having|do|does|did|doing|will|would|should|can|could|ought|i'm|you're|he's|she's|it's|we're|they're|i've|you've|we've|they've|i'd|you'd|he'd|she'd|we'd|they'd|i'll|you'll|he'll|she'll|we'll|they'll|isn't|aren't|wasn't|weren't|hasn't|haven't|hadn't|doesn't|don't|didn't|won't|wouldn't|shan't|shouldn't|can't|cannot|couldn't|mustn't|let's|that's|who's|what's|here's|there's|when's|where's|why's|how's|a|an|the|and|but|if|or|because|as|until|while|of|at|by|for|with|about|against|between|into|through|during|before|after|above|below|to|from|up|upon|down|in|out|on|off|over|under|again|further|then|once|here|there|when|where|why|how|all|any|both|each|few|more|most|other|some|such|no|nor|not|only|own|same|so|than|too|very|say|says|said|shall)$/,
     punctuation = new RegExp("[" + unicodePunctuationRe + "]", "g"),
     wordSeparators = /[ \f\n\r\t\v\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u2028\u2029\u202f\u205f\u3000\u3031-\u3035\u309b\u309c\u30a0\u30fc\uff70]+/g,
     discard = /^(@|https?:|\/\/)/,
@@ -347,7 +347,6 @@ d3.select("#random-palette").on("click", function() {
   var socket = io.connect('http://localhost:8081');
   socket.on('words', function(data)
   {
-    console.log(data);
     tags = {};
     var cases = {};
 
@@ -355,7 +354,6 @@ d3.select("#random-palette").on("click", function() {
       if (discard.test(word)) return;
       word = word.replace(".", "");
       if (stopWords.test(word.toLowerCase())) return;
-      console.log(word+' '+stopWords.test(word.toLowerCase()));
       word = word.substr(0, maxLength);
       cases[word.toLowerCase()] = word;
       tags[word = word.toLowerCase()] = (tags[word] || 0) + 1;
